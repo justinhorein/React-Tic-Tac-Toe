ff9746c9f012b56495e6af52d7f937a4c3e81854
Justin Horein <justinhorein@gmail.com>
2020-11-22 21:53:14 -0800

    game done. tie message implemented

--- src/index.js
+++ src/index.js
@@ -52,12 +52,14 @@ class Game extends React.Component {
     history: [{
       squares: Array(9).fill(null),
     }],
+    stepNumber: 0,
     xIsNext: true,
   };
 }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0,
+  this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -69,17 +71,37 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    })
+  }
+
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
-      status = 'Winner: ' + winner;
+      status = winner;
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
@@ -94,7 +116,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -116,8 +138,12 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return 'Winner: ' + squares[a];
     }
+  }
+  if (squares[0] && squares[1] && squares[2] && squares[3] && squares[4] &&
+   squares[5] && squares[6] && squares[7] && squares[8]){
+    return "It's a Tie";
   }
   return null;
 }
