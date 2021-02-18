1fbf2e7157ac3627fe94aaaec70bffadce5e8391
Justin Horein <justinhorein@gmail.com>
2021-02-17 23:56:30 -0800

    basic styling done

--- src/index.js
+++ src/index.js
@@ -107,18 +107,25 @@ class Game extends React.Component {
     }
 
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board 
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
+      <span>
+        <div class="heading">
+          <h1>Tic-Tac-Toe</h1>
         </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+
+        <div className="game">
+          <div className="game-info">
+            <div class="status">{status}</div>
+            <ol>{moves}</ol>
+          </div>
+          
+          <div className="game-board">
+            <Board 
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+          </div>
         </div>
-      </div>
+      </span>
     );
   }
 }
